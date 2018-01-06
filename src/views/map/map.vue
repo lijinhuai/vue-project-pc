9f2444f7c16447da6ee01facd839f52b511cfe19
lijinhuai <lijinhuai@vip.qq.com>
2018-01-06 21:58:04 +0800

    commit

--- src/views/map/map.vue
+++ src/views/map/map.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main">
+    <input hidden v-model="baseUrl" id="baseUrl" />
     <iframe src="static/html/map.html" style="width:100%; height:100%; border: 0px;"></iframe>
   </div>
 </template>
@@ -9,3 +10,17 @@
   height: 100%;
 }
 </style>
+<script>
+import config from '@/config/index'
+export default {
+  data () {
+    return {
+      baseUrl: ''
+    }
+  },
+  mounted () {
+    this.baseUrl = config.BASE_API
+  }
+}
+</script>
+
