59d6969983d4ae1607329c1243e49e316dca2584
lijinhuai <lijinhuai@vip.qq.com>
2018-01-11 14:34:16 +0800

    commit

---
+++ src/views/gzgl/car/components/TableCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <Card style="width:320px">
+    <div style="text-align:center">
+      <!-- <img src="../../images/logo.png"> -->
+      <h3>一套基于 Vue.js 的高质量UI组件库</h3>
+    </div>
+  </Card>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array
+    }
+  }
+}
+</script>
