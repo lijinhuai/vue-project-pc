2ed6b9e4bbdc4476e73cd840ad46d5459ab8c74f
lijinhuai <lijinhuai@vip.qq.com>
2018-03-15 17:56:49 +0800

    commit

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -37,7 +37,7 @@
                   <Icon type="arrow-down-b"></Icon>
                 </a>
                 <DropdownMenu slot="list">
-                  <DropdownItem name="ownSpace">个人中心</DropdownItem>
+                  <!-- <DropdownItem name="ownSpace">个人中心</DropdownItem> -->
                   <DropdownItem name="loginout" divided>退出登录</DropdownItem>
                 </DropdownMenu>
               </Dropdown>
