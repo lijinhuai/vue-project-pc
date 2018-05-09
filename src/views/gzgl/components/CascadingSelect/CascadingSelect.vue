4331b68941a340d7e6fd6065d1be25992ee60b61
lijinhuai <lijinhuai@vip.qq.com>
2018-05-09 17:25:44 +0800

    commit

--- src/views/gzgl/components/CascadingSelect/CascadingSelect.vue
+++ src/views/gzgl/components/CascadingSelect/CascadingSelect.vue
@@ -19,12 +19,12 @@
       </ul>
       <div v-show="a" class="address-area">
         <label class="text-success">
-              <span class="text-muted">地址：</span> {{ a }} {{ p }} {{ c }}
-          </label>
+                  <span class="text-muted">地址：</span> {{ a }} {{ p }} {{ c }}
+              </label>
       </div>
       <div class="address-option-footer">
         <!-- <button @click="close()" class="btn btn-link btn-sm">关闭</button>
-          <button @click="clear()" class="btn btn-default btn-sm">清空</button> -->
+              <button @click="clear()" class="btn btn-default btn-sm">清空</button> -->
         <button @click="confirm()" class="btn btn-success btn-sm" :disabled="!(p&&c&&a)">确定</button>
       </div>
     </div>
@@ -35,6 +35,7 @@
 <script lang="babel">
   import addressData from './CascadingSelectData.json'
   export default {
+    name: 'cascading',
     props: {
       inputClass: String,
       readonly: Boolean
@@ -58,6 +59,22 @@
       }
     },
     methods: {
+      init (a, p, c) {
+        this.setArea(a)
+        for (var i in this.pcss) {
+          var pcs = this.pcss[i]
+          if (pcs.pcsdm === p) {
+            this.setPcs(pcs)
+            for (var j in this.communitys) {
+              var community = this.communitys[j]
+              if (community.jcwdm === c) {
+                this.setCommunity(community)
+                this.confirm()
+              }
+            }
+          }
+        }
+      },
       clear () {
         this.a = ''
         this.p = ''
@@ -114,13 +131,25 @@
   box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
   position: absolute;
   z-index: 99999;
-  background: #fff;
+  /* background: #fff; */
+  /* background: rgba(2, 48, 65, 0.7); */
   width: 318px;
+  background-image: url(/static/image/marker_dynamic_bg.png);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  -moz-background-size: 100% 100%;
 }
 .cascading-address ul {
   padding: 5px;
-  border-bottom: 1px dotted #ddd;
+  /* border-bottom: 1px dotted #ddd; */
   text-align: left;
+  border-image-source: url(/static/image/line1.png);
+  border-style: solid;
+  border-width: 1px 1px;
+  border-image-slice: 1 fill;
+  border-image-outset: initial;
+  border-image-repeat: initial;
+  margin: 0 10px;
 }
 .cascading-address ul li {
   font-size: 13px;
@@ -136,9 +165,11 @@
   margin: 10px;
   font-weight: bold;
   text-align: left;
+  padding-left: 1rem;
 }
 .label {
-  padding: 0.2em 0 0.3em;
+  color: #b2bbc1;
+  padding: 0.2em 0.6em 0.3em;
   display: inline;
   line-height: 1;
   text-align: center;
@@ -148,10 +179,11 @@
 .label-success {
   font-size: 75%;
   font-weight: bold;
-  color: #fff;
+  /* color: #fff; */
+  color: #0dc2fd;
   padding: 0.2em 0.6em 0.3em;
   border-radius: 0.25em;
-  background-color: #5cb85c;
+  /* background-color: #5cb85c; */
   /* background-color:#0dc2fd; */
 }
 .btn {
@@ -182,12 +214,15 @@
   background: none;
 }
 .btn-link:hover {
-  color: #4cae4c !important;
+  /* color: #4cae4c !important; */
+  color: #0dc2fd !important;
 }
 .btn-success {
   color: #fff;
-  background-color: #5cb85c;
-  border-color: #4cae4c;
+  /* background-color: #5cb85c;
+      border-color: #4cae4c; */
+  border-color: #337ab7;
+  background-color: #21597a;
 }
 .btn-sm {
   padding: 5px 10px;
@@ -199,12 +234,13 @@
   color: #337ab7;
 }
 .text-success {
-  color: #3c763d;
+  /* color: #3c763d; */
+  color: #40c7da;
 }
 .address-option-footer {
   padding: 10px 15px;
-  background-color: #f5f5f5;
-  border-top: 1px solid #ddd;
+  /* background-color: #f5f5f5; */
+  /* border-top: 1px solid #ddd; */
   border-bottom-right-radius: 3px;
   border-bottom-left-radius: 3px;
   text-align: right;
--- src/views/gzgl/map.vue
+++ src/views/gzgl/map.vue
@@ -14,9 +14,7 @@
     <input hidden v-model="intro" id="intro" />
     <iframe id="map" :src="iframeSrc" style="width:100%; height:100%; border: 0px;overflow:hidden;"></iframe>
     <el-dialog :visible.sync="dialogTableVisible" width="318px" :show-close="false" :close-on-click-modal="false" :close-on-press-escape="false" center lock-scroll>
-      <div>
-        <cascading-address @confirm="confirmCity"></cascading-address>
-      </div>
+        <cascading-address ref="cascading" @confirm="confirmCity"></cascading-address>
     </el-dialog>
   </div>
 </template>
@@ -27,7 +25,7 @@ import cascadingAddress from './components/CascadingSelect/CascadingSelect.vue'
 export default {
   data () {
     return {
-      dialogTableVisible: false,
+      dialogTableVisible: true,
       baseUrl: '',
       facePicBaseUrl: '',
       carPicBaseUrl: '',
@@ -48,12 +46,15 @@ export default {
     cascadingAddress
   },
   mounted () {
+    const _self = this
     this.baseUrl = config.BASE_API
     this.facePicBaseUrl = config.facePicBaseUrl
     this.carPicBaseUrl = config.carPicBaseUrl
     this.mjPicBaseHost = config.mjPicBaseHost
     this.rtspServer = config.rtspServer
-    this.initDefault()
+    setTimeout(function () {
+      _self.initDefault()
+    }, 0)
   },
   methods: {
     confirmCity (data) {
@@ -63,25 +64,17 @@ export default {
       this.jcwdm = data.community.jcwdm
       this.center = data.community.center
       if (data.community.type === 'city') {
-        this.iframeSrc = 'static/html/map_city.html?jdwdm=' + data.community.jcwdm
+        this.iframeSrc =
+          'static/html/map_city.html?jdwdm=' + data.community.jcwdm
       } else {
-        this.iframeSrc = 'static/html/map_village.html?jdwdm=' + data.community.jcwdm
+        this.iframeSrc =
+          'static/html/map_village.html?jdwdm=' + data.community.jcwdm
       }
       this.intro = data.community.intro
       this.dialogTableVisible = false
     },
     initDefault () {
-      this.pcsdm = '310116560000'
-      this.jlxdm = '282719'
-      this.jcwdm = '16113006'
-      this.center = {
-        lon: '121.3377285004',
-        lat: '30.7521905824'
-      }
-      this.iframeSrc = 'static/html/map_city.html?jdwdm=16113006'
-      this.intro =
-        '万盛金邸小区位于金山区龙轩路1000弄、866弄，总占地面积22.5万平方米，居民楼栋66栋，主要出入口4个，南北向的海汇商业街将住宅小区划分为东、西两区，海汇街面积9.5万平方米。'
-      this.dialogTableVisible = false
+      this.$refs.cascading.init('金山区', '310116560000', '16113006')
     },
     switchCommunity () {
       this.dialogTableVisible = true
@@ -91,9 +84,15 @@ export default {
 </script>
 
 <style lang="less">
+body {
+  padding: 0 !important;
+}
 .main {
   height: 100%;
   .el-dialog--center {
+    .el-dialog__header {
+      display: none;
+    }
     .el-dialog__body {
       padding: 0px 0px;
     }
